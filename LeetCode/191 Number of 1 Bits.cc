69ee947c9141b3cb7c65b31ef7ce3c1178c6efe6
mgsweet <mgsweet@126.com>
2020-06-21 09:29:25 +1000

    Create 191 Number of 1 Bits.cc

---
+++ LeetCode/191 Number of 1 Bits.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; ++i) {
+            if (n & mask) {
+                count++;
+            }
+            mask = mask << 1;
+        }
+        return count;
+    }
+};
