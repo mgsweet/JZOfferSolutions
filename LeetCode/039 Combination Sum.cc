d86d2e333be8fadc80c2fdff98e55f0a5d74d602
mgsweet <mgsweet@126.com>
2020-07-14 18:33:50 +1000

    Create 039 Combination Sum.cc

---
+++ LeetCode/039 Combination Sum.cc
@@ -0,0 +1,23 @@
+class Solution {
+private:
+  void static FindAllCombination(vector<vector<int>> &ans, vector<int> &candidates, int target, int current_pos, vector<int> &current_vec) {
+    for (int i = current_pos; i < candidates.size(); ++i) {
+      current_vec.push_back(candidates[i]);
+      if (target - candidates[i] == 0) {
+        ans.push_back(current_vec);
+      } else if (target - candidates[i] > 0) {
+        FindAllCombination(ans, candidates, target - candidates[i], i, current_vec);
+      }
+      current_vec.pop_back();
+    }
+  }
+public:
+  vector<vector<int>> static combinationSum(vector<int> &candidates, int target) {
+    vector<vector<int>> ans;
+    if (candidates.empty()) return ans;
+    sort(candidates.begin(), candidates.end());
+    vector<int> current_vec;
+    FindAllCombination(ans, candidates, target, 0, current_vec);
+    return ans;
+  }
+};
