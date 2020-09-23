49129373206e18dca57f93fba63047f887d0368f
mgsweet <mgsweet@126.com>
2020-09-23 23:08:34 +1000

    Create 0303 Range Sum Query - Immutable.cc

---
+++ LeetCode/Algorithms/0303 Range Sum Query - Immutable.cc
@@ -0,0 +1,21 @@
+class NumArray {
+private:
+    vector<int> sum;
+public:
+    NumArray(vector<int>& nums) {
+        sum = vector<int>(nums.size() + 1, 0);
+        for (int i = 0; i < nums.size(); ++i) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(i,j);
+ */
