22f825d820248bc96b65b4a6ded9cc8b9b96a65e
mgsweet <mgsweet@126.com>
2020-10-11 21:14:34 +1100

    Create 0669 Trim a Binary Search Tree.cc

---
+++ LeetCode/Algorithms/0669 Trim a Binary Search Tree.cc
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  TreeNode *trimBST(TreeNode *root, const int &low, const int &high) {
+    if (!root) return nullptr;
+    root->left = trimBST(root->left, low, high);
+    root->right = trimBST(root->right, low, high);
+    if (root->val < low) return root->right;
+    else if (root->val > high) return root->left;
+    else return root;
+  }
+};
