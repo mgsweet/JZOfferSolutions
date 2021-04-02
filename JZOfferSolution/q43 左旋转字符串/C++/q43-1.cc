2cdaf4f47c4a0674c0248833ac58a141a9cc301e
mgsweet <mgsweet@126.com>
2021-04-02 17:43:53 +1100

    Update q43-1.cc

--- JZOfferSolution/q43 左旋转字符串/C++/q43-1.cc
+++ JZOfferSolution/q43 左旋转字符串/C++/q43-1.cc
@@ -1,23 +1,14 @@
 class Solution {
 public:
-    void swap_char(char &a, char &b) {
-        char tmp = a;
-        a = b;
-        b = tmp;
-    }
-    
     string LeftRotateString(string str, int n) {
-        if (n <= 0 || str.empty()) return str;
+        if (str.empty()) return str;
         n %= str.size();
-        for (int i = 0, j = n - 1; i < j; ++i, --j) {
-            swap_char(str[i], str[j]);
-        }
-        for (int i = n, j = str.size() - 1; i < j; ++i, --j) {
-            swap_char(str[i], str[j]);
-        }
-        for (int i = 0, j = str.size() - 1; i < j; ++i, --j) {
-            swap_char(str[i], str[j]);
-        }
+        for (int i = 0, j = n - 1; i < j; ++i, --j) 
+            swap(str[i], str[j]);
+        for (int i = n, j = str.size() - 1; i < j; ++i, --j) 
+            swap(str[i], str[j]);
+        for (int i = 0, j = str.size() - 1; i < j; ++i, --j)
+            swap(str[i], str[j]);
         return str;
     }
 };
