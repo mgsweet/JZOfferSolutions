f204c353affb59a3441f0a19a764925eefdaaf95
mgsweet <mgsweet@126.com>
2021-04-08 00:58:46 +1000

    finally get back to China, too tired today and choose a very simple one.

---
+++ Nowcoder/NC103-反转字符串.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /**
+     * 反转字符串
+     * @param str string字符串 
+     * @return string字符串
+     */
+    string solve(string str) {
+        reverse(str.begin(), str.end());
+        return str;
+    }
+};
