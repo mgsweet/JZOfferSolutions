57a4b9626a36d43e20788a8d399855775689cabf
mgsweet <mgsweet@126.com>
2021-04-23 14:47:34 +0800

    Update 0128 Longest Consecutive Sequence.cc

--- LeetCode/Algorithms/0128 Longest Consecutive Sequence.cc
+++ LeetCode/Algorithms/0128 Longest Consecutive Sequence.cc
@@ -3,21 +3,13 @@ public:
     static int longestConsecutive(vector<int>& nums) {
         unordered_set<int> my_set(nums.begin(), nums.end());
         int ans = 0;
+        int pre, next;
         while (!my_set.empty()) {
-          int tmp_ans = 1;
-          int cur = *my_set.begin();
-          int tmp_cur = cur;
-          while (my_set.find(--tmp_cur) != my_set.end()) {
-            my_set.erase(tmp_cur);
-            ++tmp_ans;
-          }
-          tmp_cur = cur;
-          while (my_set.find(++tmp_cur) != my_set.end()) {
-            my_set.erase(tmp_cur);
-            ++tmp_ans;
-          }
-          my_set.erase(cur);
-          ans = max(ans, tmp_ans);
+            pre = next = *my_set.begin();
+            my_set.erase(my_set.begin());
+            while (my_set.find(--pre) != my_set.end()) my_set.erase(pre);
+            while (my_set.find(++next) != my_set.end()) my_set.erase(next);
+            ans = max(ans, next - pre - 1);
         }
         return ans;
     }
