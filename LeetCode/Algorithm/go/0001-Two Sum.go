19c0bc7dd314791ae595fa3e3ffbdea0bf19722f
mgsweet <mgsweet@126.com>
2021-07-07 11:11:19 +0800

    Create 0001-Two Sum.go

---
+++ LeetCode/Algorithm/go/0001-Two Sum.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for i, v := range nums {
+        if preI, ok := m[target - v]; ok {
+            return []int{preI, i}
+        }
+        m[v] = i
+    }
+    return []int{-1, -1}
+}
